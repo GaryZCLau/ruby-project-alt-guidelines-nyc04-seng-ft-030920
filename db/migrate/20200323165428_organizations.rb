62339e58975143803bcccf12acb308be1ad7f7e0
GaryZCLau <garyzclau@gmail.com>
2020-03-23 18:46:36 -0400

    gary second

--- db/migrate/20200323165428_organizations.rb
+++ db/migrate/20200323165428_organizations.rb
@@ -1,7 +1,15 @@
 class Organizations < ActiveRecord::Migration[5.2]
   def change
     create_table :organizations do |t|
-      t.string :name
+      t.string :opportunity
+      t.string :requirements
+      t.text :content
+      t.string :summary
+      t.string :url
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :postcode
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,50 @@
 require 'httparty'
+require 'json'
+require 'pry'
+
+# bob = Volunteer.new("Bob Wu")
+# ruby = Volunteer.new("Ruby Rails")
 
 response = HTTParty.get("https://data.cityofnewyork.us/resource/shpd-5q9m.json")
-response.body
+databasearray = JSON.parse(response.body)
+
+# opportunity
+opportunity = databasearray.map do |org|
+    org["opportunity_id"]
+end
+
+# requirements
+# SANITIZE HTML TAGS
+requirements = databasearray.map do |org| 
+    org["requirements"]
+end
+
+
+content = databasearray.map do |org|
+    org["content_id"]
+end
+
+ summary = databasearray.map do |org|
+    org["summary"]
+end
+
+ url = databasearray.map do |org|
+    org["website"]
+end
+
+street_address = databasearray.map do |org|
+    org["street_address"]
+end
+
+city = databasearray.map do |org|
+    org["city"]
+end
+
+state = databasearray.map do |org|
+    org["state"]
+end
+
+postcode = databasearray.map do |org|
+    org["postcode"]
+end
+
